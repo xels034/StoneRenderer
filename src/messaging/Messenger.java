d38de86c2d5137539ccf5da6b5f2e72b4a0f4e8a
Xels034 <59102313+xels034@users.noreply.github.com>
2023-12-03 12:42:59 +0100

    - Fix warnings for latest java version

--- src/messaging/Messenger.java
+++ src/messaging/Messenger.java
@@ -1,20 +1,20 @@
 package messaging;
 
 /**
- * 
+ *
  * Messaging Hub. Any Object implementing the Handler interface may register itself
- * to this static class to receive custom-filtered messages. sending messages and 
+ * to this static class to receive custom-filtered messages. sending messages and
  * registering any objects implementing the handler interface is possible from
  * anywhere and under any circumstances inside the application.
- * 
+ *
  * Each subscriber has to offer a list of message types he's interested in. Whenever a
  * message is sent, all handlers that have announced to listen to the type of the sent
  * message will receive it.
- * 
+ *
  * Delivering of messages happens once per frame, when the update() method is called from the
  * main loop in AppWindow. Special out-of-order delivery between frames can also be forced, but
  * should be used wisely
- * 
+ *
  */
 
 import java.util.Collection;
@@ -28,33 +28,33 @@ public class Messenger {
   private static HashMap<Message.M_TYPE,LinkedList<Handler>> handleMap = new HashMap<>();
   private static LinkedList<Message> mQueue = new LinkedList<>();
 
-  
+
   public static void subscribe(Handler h, Message.M_TYPE msgT){
     if(!handleMap.containsKey(msgT)){
-      handleMap.put(msgT, new LinkedList<Handler>());
+      handleMap.put(msgT, new LinkedList<>());
     }
     handleMap.get(msgT).add(h);
   }
-  
+
   public static void subscribe(Handler h, Collection<Message.M_TYPE> msgT){
     for(Message.M_TYPE i : msgT){
       subscribe(h, i);
     }
   }
-  
+
   public static void unsubscribe(Handler h, Message.M_TYPE msgT){
     if(handleMap.containsKey(msgT)){
       handleMap.get(msgT).remove(h);
     }
   }
-  
+
   public static void unsubscribe(Handler h){
     SimpleLogger.log("Handler "+h+" unsubscribed completly", 0, Messenger.class, "unsubscribe");
     for(Message.M_TYPE msgT : handleMap.keySet()){
       handleMap.get(msgT).remove(h);
     }
   }
-  
+
   @SuppressWarnings("unchecked")
   public static void fire(Message m){
     if(handleMap.containsKey(m.getMsgType())){
@@ -66,11 +66,11 @@ public class Messenger {
       SimpleLogger.log("Note: Message of type "+m.getMsgType()+" is requested by no-one!", 1, Messenger.class, "fire");
     }
   }
-  
+
   public static void send(Message m){
     mQueue.add(m);
   }
-  
+
   @SuppressWarnings("unchecked")
   public static void update(){
     LinkedList<Message> workList = (LinkedList<Message>)mQueue.clone();
